95fc5a585e83b5dd76333e9c95450ba17eee1de0
Arsh Goel <arshgoel16@gmail.com>
2025-08-19 20:48:33 +0530

    updates

--- Foxxy_Drip/urls.py
+++ Foxxy_Drip/urls.py
@@ -3,11 +3,13 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-
+from Accounts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('Accounts.urls')),
     path('dash/', include('Services.urls')),
     path('',TemplateView.as_view(template_name = "home.html"), name='home'),
     path("accounts/",include("allauth.urls")),
+    path('aboutus',views.aboutus,name='aboutus'),
+    path('contactus',views.contactus,name='contactus')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
