0a85065ecc19d4c018766b13487b7efea8efcd74
Arsh Goel <arshgoel16@gmail.com>
2025-08-24 18:50:19 +0530

    OPT

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # Start Gunicorn (bind to platform PORT; default 8000 for local)
-exec gunicorn YOUR_PROJECT.wsgi:application \
+exec gunicorn Foxxy_Drip.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
   --workers ${GUNICORN_WORKERS:-2} \
   --threads ${GUNICORN_THREADS:-2} \
