9902abf1ed10c3aa49e508fe31ad48b041a5094a
Arsh Goel <arshgoel16@gmail.com>
2026-01-04 00:46:24 +0530

    Try

--- Foxxy_Drip/settings.py
+++ Foxxy_Drip/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'x7qba81wr%o2d31pp_m(i*!pah(5&5)o)8h7oaybd&pixn!0ki'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
